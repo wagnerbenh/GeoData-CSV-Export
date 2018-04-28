427ffeddaf8062d5013b43896301edc20118fe03
yankees9920 <wagner.benh@gmail.com>
2018-04-28 19:57:56 -0400

    Update export.js

--- export.js
+++ export.js
@@ -1,17 +1,17 @@
 var array = [["Address", "Type", "Class", "Document Date", "Owner", "Plaintiff", "Contact Name", "Contact Telephone", "Contact Type"]];
 
 $("tr#trselectprop").each(function() {
-  var address = $(this).find("td#tdAddress")[0].innerText.replace(/(\r\n\t|\n|\r\t)/gm,"");
+  var address = $(this).find("td#tdAddress")[0].innerText.replace(/(\r\n\t|\n|\r\t)/gm,"").replace(/,/g, " ");
   var type = $(this).find("td#tdtype")[0].innerText;
   var property_class = $(this).find("td#tdClass")[0].innerText;
   var doc_date = $(this).find("td#tdDocDate")[0].innerText;
-  var owner = $(this).find("td#tdOwner")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(",", " ");
-  var plaintiff = $(this).find("td#tdPlaintiff")[0].innerText.replace(",", " ");
+  var owner = $(this).find("td#tdOwner")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(/,/g, " ");
+  var plaintiff = $(this).find("td#tdPlaintiff")[0].innerText.replace(/,/g, " ");
 
 	$(this).next().find("tbody tr").each(function() {
   	var property_array = [];
     
-    var contact_name = $(this).find("td")[0].innerText.replace(",", " "); 
+    var contact_name = $(this).find("td")[0].innerText.replace(/,/g, " "); 
     var contact_telephone = $(this).find("td")[1].innerText;
     var contact_type = $(this).find("td")[2].innerText;
     
@@ -26,6 +26,8 @@ $("tr#trselectprop").each(function() {
 var csv = array.map(function(d){
 	return d.join();
 }).join('\n');
+
+console.log(csv);
 
 /* export to CSV file */
 var pom = document.createElement('a');
