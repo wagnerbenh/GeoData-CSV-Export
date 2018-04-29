647757d622f65f7319ee03130106f92dec5bf11f
yankees9920 <wagner.benh@gmail.com>
2018-04-29 10:22:00 -0400

    support the different formats for different areas

--- export.js
+++ export.js
@@ -1,17 +1,62 @@
-var array = [["Street Address", "City", "Zip Code", "Type", "Class", "Document Date", "Owner", "Plaintiff", "Contact Name", "Contact Telephone", "Contact Type"]];
+var array = [["Street Address", "City", "Zip Code", "Type", "Class", "Document Date", "Amount", "Owner", "Plaintiff", "Contact Name", "Contact Telephone", "Contact Type"]];
 
 $("tr#trselectprop").each(function() {
-  var address = $(this).find("td#tdAddress")[0].innerText.replace(/,/g, " ");
-
+  
+  if ($(this).find("td#tdAddress")[0] != undefined) {
+  	var address = $(this).find("td#tdAddress")[0].innerText.replace(/,/g, " ");
+  } else if ($(this).find("td#Neighborhood")[0] != undefined) {
+  	var address = $(this).find("td#Neighborhood")[0].innerText.replace(/,/g, " ");
+  } else {
+  	var address = null;
+  }
+  
   var street_address = address.substr(0, address.lastIndexOf("\n"));
   var city = address.substring(address.lastIndexOf("\n") + 1,address.length - 6);
   var zip = address.substr(address.length - 5);
 	
-  var type = $(this).find("td#tdtype")[0].innerText;
-  var property_class = $(this).find("td#tdClass")[0].innerText;
-  var doc_date = $(this).find("td#tdDocDate")[0].innerText;
-  var owner = $(this).find("td#tdOwner")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(/,/g, " ");
-  var plaintiff = $(this).find("td#tdPlaintiff")[0].innerText.replace(/,/g, " ");
+  if ($(this).find("td#tdtype")[0] != undefined) {
+  	var type = $(this).find("td#tdtype")[0].innerText;
+  } else {
+  	var type = null;
+  }
+  
+  if ($(this).find("td#tdClass")[0] != undefined) {
+  	var property_class = $(this).find("td#tdClass")[0].innerText;
+  } else if ($(this).find("#Class")[0] != undefined) {
+  	var property_class = $(this).find("#Class")[0].innerText;
+  } else {
+    var property_class = null;
+  }
+  
+  if ($(this).find("td#tdDocDate")[0] != undefined) {
+  	var doc_date = $(this).find("td#tdDocDate")[0].innerText;
+  } else if ($(this).find("#DocDate")[0] != undefined) {
+    var doc_date = $(this).find("#DocDate")[0].innerText;
+  } else {
+  	var doc_date = null;
+  }
+
+  if ($(this).find("#Amount")[0] != undefined) {
+  	var amount = $(this).find("#Amount")[0].innerText.replace(/,/g, "");
+  } else {
+  	var amount = null;
+  }
+
+  if ($(this).find("td#tdOwner")[0] != undefined) {
+  	var owner = $(this).find("td#tdOwner")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(/,/g, " ");
+  } else if ($(this).find("#Owner")[0] != undefined) {
+  	var owner = $(this).find("#Owner")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(/,/g, " ");
+  } else {
+  	var owner = null;
+  }
+  
+  if ($(this).find("td#tdPlaintiff")[0] != undefined) {
+  	var plaintiff = $(this).find("td#tdPlaintiff")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"");
+  } else if ($(this).find("#Plaintiff")[0] != undefined){
+    var plaintiff = $(this).find("#Plaintiff")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"");
+  } else {
+  	var plaintiff = null;
+  }
 
   $(this).next().find("tbody tr").each(function() {
     var property_array = [];
@@ -20,7 +65,7 @@ $("tr#trselectprop").each(function() {
     var contact_telephone = $(this).find("td")[1].innerText;
     var contact_type = $(this).find("td")[2].innerText;
     
-    property_array.push(street_address, city, zip, type, property_class, doc_date, owner, plaintiff, contact_name, contact_telephone, contact_type);
+    property_array.push(street_address, city, zip, type, property_class, doc_date, amount, owner, plaintiff, contact_name, contact_telephone, contact_type);
     
     array.push(property_array);
   });
