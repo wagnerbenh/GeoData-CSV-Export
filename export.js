32cef2b6f78e1d0fa02610c99a5f68ca7a4c1342
yankees9920 <wagner.benh@gmail.com>
2018-04-28 23:45:05 -0400

    separated address into street, city, zip and components

--- export.js
+++ export.js
@@ -1,21 +1,26 @@
-var array = [["Address", "Type", "Class", "Document Date", "Owner", "Plaintiff", "Contact Name", "Contact Telephone", "Contact Type"]];
+var array = [["Street Address", "City", "Zip Code", "Type", "Class", "Document Date", "Owner", "Plaintiff", "Contact Name", "Contact Telephone", "Contact Type"]];
 
 $("tr#trselectprop").each(function() {
-  var address = $(this).find("td#tdAddress")[0].innerText.replace(/(\r\n\t|\n|\r\t)/gm,"").replace(/,/g, " ");
+  var address = $(this).find("td#tdAddress")[0].innerText.replace(/,/g, " ");
+
+  var street_address = address.substr(0, address.lastIndexOf("\n"));
+  var city = address.substring(address.lastIndexOf("\n") + 1,address.length - 6);
+  var zip = address.substr(address.length - 5);
+	
   var type = $(this).find("td#tdtype")[0].innerText;
   var property_class = $(this).find("td#tdClass")[0].innerText;
   var doc_date = $(this).find("td#tdDocDate")[0].innerText;
   var owner = $(this).find("td#tdOwner")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(/,/g, " ");
   var plaintiff = $(this).find("td#tdPlaintiff")[0].innerText.replace(/,/g, " ");
 
-	$(this).next().find("tbody tr").each(function() {
-  	var property_array = [];
+  $(this).next().find("tbody tr").each(function() {
+    var property_array = [];
     
     var contact_name = $(this).find("td")[0].innerText.replace(/,/g, " "); 
     var contact_telephone = $(this).find("td")[1].innerText;
     var contact_type = $(this).find("td")[2].innerText;
     
-    property_array.push(address, type, property_class, doc_date, owner, plaintiff, contact_name, contact_telephone, contact_type);
+    property_array.push(street_address, city, zip, type, property_class, doc_date, owner, plaintiff, contact_name, contact_telephone, contact_type);
     
     array.push(property_array);
   });
