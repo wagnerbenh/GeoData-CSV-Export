922626849b7e159bf6c366775b50bb8141ddbb9e
wagnerbenh <wagner.benh@gmail.com>
2018-05-20 12:26:48 -0400

    added support for auctions

--- export.js
+++ export.js
@@ -1,15 +1,17 @@
-var array = [["Street Address", "City", "Zip Code", "Type", "Class", "Document Date", "Amount", "Owner", "Plaintiff", "Contact Name", "Contact Telephone", "Contact Type"]];
+var array = [["Street Address", "City", "Zip Code", "Type", "Class", "Document Date", "Amount", "Owner", "Plaintiff", "Auction Info", "Borrower", "Lender", "Contact Name", "Contact Telephone", "Contact Type"]];
 
 $("tr#trselectprop").each(function() {
-  
+  console.log("in loop");
   if ($(this).find("td#tdAddress")[0] != undefined) {
   	var address = $(this).find("td#tdAddress")[0].innerText.replace(/,/g, " ");
   } else if ($(this).find("td#Neighborhood")[0] != undefined) {
   	var address = $(this).find("td#Neighborhood")[0].innerText.replace(/,/g, " ");
+  } else if ($(this).find("#Address")[0] != undefined) {
+  	var address = $(this).find("#Address")[0].innerText.replace(/,/g, " ");
   } else {
   	var address = null;
   }
-  
+
   var street_address = address.substr(0, address.lastIndexOf("\n"));
   var city = address.substring(address.lastIndexOf("\n") + 1,address.length - 6);
   var zip = address.substr(address.length - 5);
@@ -24,6 +26,8 @@ $("tr#trselectprop").each(function() {
   	var property_class = $(this).find("td#tdClass")[0].innerText;
   } else if ($(this).find("#Class")[0] != undefined) {
   	var property_class = $(this).find("#Class")[0].innerText;
+  } else if ($(this).find("#Classification")[0] != undefined) {
+  	var property_class = $(this).find("#Classification")[0].innerText;
   } else {
     var property_class = null;
   }
@@ -57,6 +61,24 @@ $("tr#trselectprop").each(function() {
   } else {
   	var plaintiff = null;
   }
+  
+  if ($(this).find("#tdauctiondate")[0] != undefined) {
+  	var auction_info = $(this).find("#tdauctiondate")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm," ").replace(/,/g, " ");
+  } else {
+  	var auction_info = null;
+  }
+  
+  if ($(this).find("#Borrower")[0] != undefined) {
+  	var borrower = $(this).find("#Borrower")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(/,/g, " ");
+  } else {
+  	var borrower = null;
+  }
+  
+  if ($(this).find("#Lender")[0] != undefined) {
+  	var lender = $(this).find("#Lender")[0].innerText.replace(/(\r\n\t|\n|\r\t\,)/gm,"").replace(/,/g, " ");
+  } else {
+  	var lender = null;
+  }
 
   $(this).next().find("tbody tr").each(function() {
     var property_array = [];
@@ -65,7 +87,7 @@ $("tr#trselectprop").each(function() {
     var contact_telephone = $(this).find("td")[1].innerText;
     var contact_type = $(this).find("td")[2].innerText;
     
-    property_array.push(street_address, city, zip, type, property_class, doc_date, amount, owner, plaintiff, contact_name, contact_telephone, contact_type);
+    property_array.push(street_address, city, zip, type, property_class, doc_date, amount, owner, plaintiff, auction_info, borrower, lender, contact_name, contact_telephone, contact_type);
     
     array.push(property_array);
   });
@@ -76,6 +98,8 @@ $("tr#trselectprop").each(function() {
 var csv = array.map(function(d){
 	return d.join();
 }).join('\n');
+
+/* console.log(csv); */
 
 /* export to CSV file */
 var pom = document.createElement('a');
